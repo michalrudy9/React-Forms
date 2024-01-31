7b45958248f24100e1c4f3ef012c8a06a781c601
michalrudy9 <michallrudy9@gmail.com>
2024-01-31 21:24:20 +0100

    Added data form object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Login from "./components/StateLogin";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
