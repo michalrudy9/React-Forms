dbb9caccb47894cdd0dfdd6a75ce193babd62fcb
michalrudy9 <michallrudy9@gmail.com>
2024-01-31 20:50:56 +0100

    Added form state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
+
 function Login() {
+  const [enteredData, setEnteredData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredData.email);
+    console.log(enteredData.password);
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setEnteredData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={enteredData.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={enteredData.password}
+          />
         </div>
       </div>
 
